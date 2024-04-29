dac8eccc63543900d7e97f1781282838fcd147ec
Mayuri <patelmayuri400@gmail.com>
2024-04-29 19:29:18 +0530

    Added firebase setup for android.

---
+++ lib/database_helper/database_firebase.dart
@@ -0,0 +1,74 @@
+
+import 'dart:isolate';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:notes_app/database_helper/database_helper.dart';
+import 'package:notes_app/model/note_model.dart';
+import 'package:uuid/uuid.dart';
+
+class Database {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String userCollection = "users";
+  final String noteCollection = "notes";
+
+  // Future<bool> createNewUser(UserModel user) async {
+  //   try {
+  //     await _firestore
+  //         .collection(userCollection)
+  //         .doc(user.id)
+  //         .set({"id": user.id, "name": user.name, "email": user.email});
+  //     return true;
+  //   } catch (e) {
+  //     print(e);
+  //     return true;
+  //   }
+  // }
+  //
+  // Future<UserModel> getUser(String uid) async {
+  //   try {
+  //     DocumentSnapshot doc =
+  //     await _firestore.collection(userCollection).doc(uid).get();
+  //     return UserModel.fromDocumentSnapshot(doc);
+  //   } catch (e) {
+  //     print(e);
+  //     rethrow;
+  //   }
+  // }
+
+
+  Future<void> uploadData()async{
+    List<Note> notes=await DatabaseHelper.instance.getNoteList();
+    await Isolate.run(() => _deleteCollection());
+    await Isolate.run(() => addNote(notes));
+
+  }
+  Future<void> addNote(List<Note> notes) async {
+    try {
+      await Future.forEach(notes, (item) async {
+        var uuid = const Uuid().v4();
+        await _firestore.collection('notes').add({
+          "id": uuid,
+          "title": item.title,
+          "body": item.content,
+          "creationDate": Timestamp.now(),
+        });
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+
+  void _deleteCollection() async {
+    final firestore = FirebaseFirestore.instance;
+    final collectionRef = firestore.collection('notes');
+
+    // Get all documents in the collection
+    final querySnapshot = await collectionRef.get();
+
+    // Delete each document in the collection
+    final futures = querySnapshot.docs.map((doc) => doc.reference.delete());
+    await Future.wait(futures);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:notes_app/routing/app_pages.dart';
 import 'package:notes_app/routing/app_routes.dart';
 
-main() {
+main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyC4U0d8OQnm0-qqz7Hk5n2YGktrpP21yRw',
+        appId: '1:130370578184:android:2f5283e77e7afe3a28aebd ',
+        messagingSenderId: 'sendid',
+        projectId: 'noteapp-dedc8',
+      )
+  );
   runApp(MyApp());
 }
 
